426c9bc10f82d5e50d7051e68afcc705eadb0397
aiiro <130713451+pcdobebo@users.noreply.github.com>
2023-04-14 00:36:46 -0300

    Add files via upload
    Disciplinas agora são escolhidas pelo usuário.
    Adicionado um sistema de tentativas de login.
    Correção de bugs onde o código era infinito.
    
    //Ainda necessita de correções de bugs no cadastro/login.

--- boletimres.c
+++ boletimres.c
@@ -12,7 +12,7 @@ struct cadastro{ //login cadastrado do user
 };
 struct boletim{ //estrutura do boletim
     char nome[30];
-    char disciplina[20];
+    int disciplina;
     float notas[3];
     float media;
 };
@@ -25,52 +25,90 @@ int main(){
     
     int cad_ver = 0; //verificação inicia falsa
 
-    int login_ver; //verificação do login
     int fim = 1; //variavel de parada do algoritmo
     int i; //contagem de for para as notas
+    int fazerCad = 0;
+    int cadastramento = 1;
+    int tentativas;
 
     while(fim == 1){
         printf("Fazer login ou cadastro\n");
-        printf("1 - Login // 2 - Cadastro\n");
+        printf("1 - Login    2 - Cadastro\n");
         scanf("%d", &cad_ver);
-        while(cad_ver == 2){
-            printf("Digite o nome de usuario para cadastro\n");
-            scanf("%s", c.user);
-            printf("Digite a senha\n");
-            scanf("%s", c.pass);
-            printf("\nUsuario cadastrado\n");
-            cad_ver = 1;
-        }
-        if(cad_ver == 1){ //usuario tem cadastro
-            do{
-                printf("Usuario:");
-                scanf("%s", v.user_ver);
-                printf("Senha:");
-                scanf("%s", v.pass_ver);
-                if((strcmp(v.user_ver, c.user) == 0) && (strcmp(v.pass_ver, c.pass) == 0)){
-                    printf("\nLogin autorizado\n");
-                    login_ver = 1;
-                }else{
-                    printf("\nUsuario ou Senha incorreto(s)\n");
-                    login_ver = 0;
+        while(cadastramento == 1){
+            if(cad_ver == 2){
+                printf("\nDigite o nome de usuario para cadastro: ");
+                scanf("%s", c.user);
+                printf("Digite a senha: ");
+                scanf("%s", c.pass);
+                printf("\n---- Usuario cadastrado ----\n");
+                cad_ver = 1;
+            }
+            else{ //verificação do cadastro
+                int login_ver = 0; //verificação do login
+                if(login_ver != 1){
+                    for(tentativas = 1; tentativas <= 3; tentativas++){
+                        printf("\nUsuario: ");
+                        scanf("%s", v.user_ver);
+                        printf("Senha: ");
+                        scanf("%s", v.pass_ver);
+                        if((strcmp(v.user_ver, c.user) == 0) && (strcmp(v.pass_ver, c.pass) == 0)){ //usuario fez login
+                            printf("\n---- Login autorizado ----\n");
+                            login_ver = 1;
+                            cadastramento = 0;
+                            tentativas = 4;
+                        }else{ //usuario errou ou não tem cadastro
+                            printf("\nUsuario ou Senha incorreto(s)\n");
+                            login_ver = 0;
+                        }
+                    }
+                    if(login_ver == 0){
+                        printf("\nDeseja fazer cadastro?\n");
+                        printf("1 - Sim    2 - Não\n");
+                        scanf("%d", &fazerCad);
+                        if(fazerCad == 1){ //redirecionando para realização do cadastro
+                            cad_ver = 2;
+                            login_ver = 1;
+                        }else{
+                            return 0; //fim do algoritmo
+                        }
+                    }    
                 }
-            }while(login_ver != 1);
+            }
         }
-            
-        printf("Digite seu nome\n");
-        scanf("%s", b.nome);
+        printf("\nDigite seu nome\n");
+        scanf("%s[^\n]", b.nome);
         printf("\nDigite a disciplina\n");
-        scanf("%s", b.disciplina);
-        printf("\nDigite as notas do aluno\n");
+        printf("0 - Logica Matematica\n1 - Logica de Programacao\n2 - ICC\n3 - Ingles\n4 - Calculo\n");
+        scanf("%d", &b.disciplina);
+        printf("\nDigite as notas das 3 APs\n");
         for(i = 0; i <= 2; i++){
             scanf("%f", &b.notas[i]);
             if((b.notas[i] < 0) || (b.notas[i] > 10)){
                 printf("Nota inválida\n");
+                break;
             }
         }
 
         b.media = (b.notas[0] + b.notas[1] + b.notas[2]) / 3; //calculo da media das notas
-        printf("\nA media do aluno: %.1f\n", b.media);
+        switch(b.disciplina){
+            case 0:
+                printf("\nDisciplina: Logica Matematica\n");
+            break;
+            case 1:
+                printf("\nDisciplina: Logica de Programacao\n");
+            break;
+            case 2:
+                printf("\nDisciplina: ICC\n");
+            break;
+            case 3:
+                printf("\nDisciplina: Ingles\n");
+            break;
+            case 4:
+                printf("\nDisciplina: Calculo\n");
+            break;
+        }
+        printf("\nA media do aluno %s: %.1f\n", b.nome, b.media);
         if(b.media >= 7.0){
             printf("Aprovado\n");
         }else{
@@ -80,10 +118,10 @@ int main(){
                 printf("Reprovado\n");
             }
         }
-        printf(" Deseja realizar novamente?\n1 - Sim   2 - Não "); //checagem de parada do algoritmo
+        printf("\nDeseja realizar novamente?\n1 - Sim   2 - Nao\n"); //checagem de parada do algoritmo
         scanf("%d", &fim);
         if(fim != 1){
-            printf(" -- SAINDO DO SISTEMA -- ");
+            printf("\n---- SAINDO DO SISTEMA ----");
         }
     }
 
